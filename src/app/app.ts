d0c3b5d213aac1a337ad5c6df8366ad279b38861
Dage10 <davidgelmacorral@gmail.com>
2025-12-04 13:25:57 +0100

    Exercici 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,11 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('AngularDOMDavidGelmaCorral');
+
+  ngOnInit() {
+    let element = document.getElementById('text');
+    element!.innerHTML = element!.innerHTML.toUpperCase()
+  }
 }
